cd632b82507dea1eb6be3648a6edc26dba9cfc35
Gustavo Bethelmy <tenchu96@gmail.com>
2018-10-15 19:35:20 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// my age in dog years
+const myAge = 29
+let earlyYears = 2 
+let laterYears = myAge - 2
+let myName = 'Gustavo'.toLowerCase()
+earlyYears *= 10.5
+laterYears *= 4
+console.log(earlyYears)
+console.log(laterYears)
+myAgeInDogYears = earlyYears + laterYears
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`)
